1b95cc1d32cdc862c5059a8a5cd0564c74755ba9
Kearney <email@kearneytech.com>
2024-09-10 20:52:17 -0700

    Cube work on Tile functionality.

--- src/app/cube/tile.tsx
+++ src/app/cube/tile.tsx
@@ -1,5 +1,7 @@
 export default function Tile({color}: {color: string}) {
     return (
-        <div className={'border border-slate-600 w-24 h-24 cursor-pointer ' + color}></div>
+        <div className={'border border-slate-600 w-24 h-24 cursor-pointer ' + color}>
+            <button></button>
+        </div>
     );
 }
