f3be0a69da816c423cd8c43bf80102b56b53b132
Kearney <email@kearneytech.com>
2024-09-11 20:23:17 -0700

    Cube initialize tiles with random color.

--- src/app/cube/page.tsx
+++ src/app/cube/page.tsx
@@ -1,7 +1,5 @@
 /**
  * TODOs
- * Randomize colors
- * Set color sequence
  * Tile click functionality
  * Cube solved check function
  * Create tests
@@ -16,23 +14,41 @@ const tileColors = [
     'bg-[#ff0000]'
 ];
 
+type TileObject = {
+    color: string;
+};
+
+let tile1 = {color: ''} as TileObject,
+    tile2 = {color: ''} as TileObject,
+    tile3 = {color: ''} as TileObject,
+    tile4 = {color: ''} as TileObject;
+
+function initTiles(){
+    tile1.color = tileColors[randomNumber(4)];
+    tile2.color = tileColors[randomNumber(4)];
+    tile3.color = tileColors[randomNumber(4)];
+    tile4.color = tileColors[randomNumber(4)];
+};
+
+function randomNumber(max: number){
+    return Math.floor(Math.random() * max);
+}
+
 function tileClick(e: any){
     console.log('say something', e);
 }
 
-
-
 export default function Page() {
-    let i = 0;
-    const tiles = tileColors.map(tileColor =>
-        <Tile key={i++} color={tileColor} tileClick={tileClick}/>
-    )
+    initTiles();
 
     return (
         <div className="flex flex-col justify-between w-96">
             <h2>Cube</h2>
             <div className="flex flex-wrap justify-center w-64 place-self-center">
-                {tiles}
+                <Tile color={tile1.color} tileClick={tileClick}/>
+                <Tile color={tile2.color} tileClick={tileClick}/>
+                <Tile color={tile3.color} tileClick={tileClick}/>
+                <Tile color={tile4.color} tileClick={tileClick}/>
             </div>
         </div>
     );
