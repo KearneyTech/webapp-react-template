259f9b65e876247c9bde63a7549347091776b3a9
Kearney <email@kearneytech.com>
2024-09-24 15:43:52 -0700

    Goodbye Next.

--- src/app/cube/tile.tsx
+++
@@ -1,5 +0,0 @@
-export default function Tile({color, tileClick}: {color: string, tileClick: Function}) {
-    return (
-        <div onClick={tileClick} className={'border border-slate-600 w-24 h-24 cursor-pointer ' + color}></div>
-    );
-}
