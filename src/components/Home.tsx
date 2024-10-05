e582cfb1f54613a14b8150a51e2641632bc0ac7d
Kearney <email@kearneytech.com>
2024-10-04 21:41:19 -0700

    Material UI updates.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,27 @@
+import { Button, CardActions, CardContent } from '@mui/material';
+import Card from '@mui/material/Card';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-            <div>
-                <a href="/cube">Cube</a>
-                <a href="/questions">Questions</a>
-            </div>
-        </div>
+        <>
+            <Card variant='outlined'>
+                <CardContent>
+                    <h1>Home</h1>
+                    <p>
+                        This is a place for me to play with a ReactJS front end.
+                    </p>
+                </CardContent>
+                <CardActions>
+                    <Button LinkComponent={Link} to='cube'>
+                        Cube
+                    </Button>
+                    <Button LinkComponent={Link} to='questions'>
+                        Questions
+                    </Button>
+                </CardActions>
+            </Card>
+        </>
     );
 }
